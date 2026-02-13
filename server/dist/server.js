980c217cd276f97992116f0ba075efd5dafd3377
vzaleto <vzaleto@gmail.com>
2026-02-13 17:40:21 +0200

    setting render

--- server/dist/server.js
+++ server/dist/server.js
@@ -5,10 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const ws_1 = require("ws");
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const PORT = Number(process.env.PORT) || 8080;
+const BUILD_PATH = path_1.default.join(__dirname, 'build'); //
 const server = http_1.default.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('OK');
+    let filePath = path_1.default.join(BUILD_PATH, req.url && req.url != '/' ? req.url : 'index.html');
+    if (!fs_1.default.existsSync(filePath) || fs_1.default.statSync(filePath).isDirectory()) {
+        filePath = path_1.default.join(BUILD_PATH, 'index.html');
+    }
+    fs_1.default.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not Found');
+        }
+        else {
+            res.writeHead(200);
+            res.end(data);
+        }
+    });
 });
 const wss = new ws_1.WebSocketServer({ server });
 const paramsData = [
